85bc0f5da4eea4e37badcdcaf00a8216537ebb53
Iván Munguía <ivanalejandro249@gmail.com>
2018-03-22 16:53:23 -0600

    Move the token value to the utils file as a constant

--- src/components/IndicesHistoricChart.js
+++ src/components/IndicesHistoricChart.js
@@ -7,7 +7,7 @@ import {
     LineSeries
   } from 'react-vis';
   import axios from 'axios';
-import { HOST, objectToQueryString } from '../utils';
+import { HOST, TOKEN, objectToQueryString } from '../utils';
 
 class IndicesHistoricChart extends Component {
     constructor() {
@@ -20,10 +20,9 @@ class IndicesHistoricChart extends Component {
 
     componentWillMount() {
         const url = `${HOST}/graphics/indices/historic`;
-        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiI5NjJkNDI5My0yYTFjLTQyNDMtODZkOS04MzRmNjE0ZmNiZDUiLCJzZWNyZXQiOiIzYWRiYWYwZC03NjEyLTRlMDAtYjhiOC1mOTBlMmFkOTQyYWQiLCJpYXQiOjE1MjE3NDAzODQsImV4cCI6MTUyMjM0NTE4NH0.GH6pS6-C9c9f3yDH8KhLq8UdkF9befFEM70Nm-HnIsY';
-        
+
         const params = {
-          'token': token,
+          'token': TOKEN,
           'locs': ['ps3VSmIBLhTQ-ucWIt2G', 'yM3VSmIBLhTQ-ucWlt7l'],
           'index': ['structural', 'dynamic'],
           'size': '100'
@@ -40,7 +39,6 @@ class IndicesHistoricChart extends Component {
     }
 
     getPoints = (location) => {
-        // console.log(location['dynamic_index']);
         let points = location['dynamic_index'].map((points, index) => ({x: index, y: points.y}));
         console.log(points);
         return points;
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 export const HOST = 'http://localhost:5001/api';
+export const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiI5NjJkNDI5My0yYTFjLTQyNDMtODZkOS04MzRmNjE0ZmNiZDUiLCJzZWNyZXQiOiIzYWRiYWYwZC03NjEyLTRlMDAtYjhiOC1mOTBlMmFkOTQyYWQiLCJpYXQiOjE1MjE3NDAzODQsImV4cCI6MTUyMjM0NTE4NH0.GH6pS6-C9c9f3yDH8KhLq8UdkF9befFEM70Nm-HnIsY';
 
 export function objectToQueryString(params) {
     return Object.keys(params).map(key => `${key}=${params[key]}`).join('&');
